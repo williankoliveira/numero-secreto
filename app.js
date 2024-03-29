81223df8a7986c6752ff00131dcf7652bb155de5
Willian Oliveira <williankauanoliveira@outlook.com>
2024-03-29 15:02:51 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,34 @@
+alert ('Bem vindo ao jogo do número secreto');
+
+           let numeroMaximo = 100;
+
+           let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+
+           console.log (numeroSecreto);
+
+           let chute;
+
+           let tentativas = 1;
+
+while (chute != numeroSecreto)
+
+{  chute = prompt (`Escolha um número entre 1 e ${numeroMaximo}`);
+
+           if (chute == numeroSecreto)
+
+           { break; }
+
+   else {  if (chute > numeroSecreto)
+
+           { alert (`Você errou, o número secreto é menor que ${chute}`); }
+
+           else { alert (`Você errou, o número secreto é maior que ${chute}`); }
+
+           tentativas++;
+
+        }
+}
+
+           let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+
+           alert (`Parabéns, você acertou o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
